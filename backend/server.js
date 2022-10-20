35fd8da0ed0bb86d75498600168203d5782baa16
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-10-19 23:40:42 -0400

    create server with express. Add connection to localhost and require middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const connectDB = require("./config/database");
+const cors = require('cors');
+const mainRoutes = require("./routes/main");
+
+// Use Cors
+app.use(cors())
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -16,6 +22,13 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+
+// Setup Routes
+app.use('/', mainRoutes)
+
+// Server Running
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}, you better catch it!`);
   });
