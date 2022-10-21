e1ee93adcafb71d8f313b273cbc6dbd41bc79789
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-10-20 23:42:50 -0400

    add a main route/controller for /. Add a middleware folder for multer. Connected MongoDB annected to MongoDB and add a task model

---
+++ backend/controllers/home.js
@@ -0,0 +1,29 @@
+const Task = require("../models/Task");
+
+module.exports={
+    // @desc Get index (homepage)
+    // @route Get /api
+    // @access Public
+    getIndex: async (req,res) => {
+        try{
+            const tasks = await Task.find()
+            res.send({tasks: tasks});
+        } catch(err) {
+            console.log(err);
+        }
+        
+    },
+    createTask: async (req,res) => {
+        try {
+            await Task.create({
+                task: req.body.task,
+                day: req.body.day,
+                reminder: 'false',
+            })
+            console.log("Post has been added!");
+            res.redirect("/");
+        } catch (err) {
+          console.log(err);
+        }
+    },
+}
